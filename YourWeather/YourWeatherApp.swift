10532cebf7190afdad319b8f608ce5c41c2a3f0a
caiqfrrz <caiqfrrz@gmail.com>
2024-08-23 22:18:21 -0300

    Initial Commit

---
+++ YourWeather/YourWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourWeatherApp.swift
+//  YourWeather
+//
+//  Created by Caique Ferraz on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct YourWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
