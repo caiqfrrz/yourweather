570129aed47224bcb9e623c50ac8953f44393990
caiqfrrz <caiqfrrz@gmail.com>
2024-08-26 16:43:36 -0300

    first commit

---
+++ YourWeather/ApiHandling.swift
@@ -0,0 +1,8 @@
+//
+//  ApiHandling.swift
+//  YourWeather
+//
+//  Created by Caique Ferraz on 24/08/24.
+//
+
+import Foundation
---
+++ YourWeather/Forecast.swift
@@ -0,0 +1,8 @@
+//
+//  Forecast.swift
+//  YourWeather
+//
+//  Created by Caique Ferraz on 23/08/24.
+//
+
+import Foundation
