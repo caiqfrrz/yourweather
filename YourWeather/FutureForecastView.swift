9f112137c9045027d5210994f5ae3050de35bf4a
caiqfrrz <caiqfrrz@gmail.com>
2024-08-26 21:49:16 -0300

    done some UI and more stuff

---
+++ YourWeather/FutureForecastView.swift
@@ -0,0 +1,18 @@
+//
+//  FutureForecastView.swift
+//  YourWeather
+//
+//  Created by Caique Ferraz on 26/08/24.
+//
+
+import SwiftUI
+
+struct FutureForecastView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FutureForecastView()
+}
